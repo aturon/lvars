c9d12be8cf8a5d8b6ef346cbd98379d832bd38d7
Aaron Turon <turon@mpi-sws.org>
2013-05-07 18:10:52 +0200

    starting to separate out scheduling in idempotent version

---
+++ haskell-prototype/Sched.hs
@@ -0,0 +1,69 @@
+module Sched where
+
+data Sched = Sched
+    { no       :: {-# UNPACK #-} !Int,
+      workpool :: IORef [Trace],
+      idle     :: IORef [MVar Bool],
+      scheds   :: [Sched] -- Global list of all per-thread workers.
+    }
+
+-- | Process the next item on the work queue or, failing that, go into
+-- work-stealing mode.
+reschedule :: Sched -> IO ()
+reschedule queue@Sched{ workpool } = do
+  e <- atomicModifyIORef workpool $ \ts ->
+         case ts of
+           []      -> ([], Nothing)
+           (t:ts') -> (ts', Just t)
+  case e of
+    Nothing -> steal queue
+    Just t  -> sched True queue t
+
+-- RRN: Note -- NOT doing random work stealing breaks the traditional
+-- Cilk time/space bounds if one is running strictly nested (series
+-- parallel) programs.
+
+-- | Attempt to steal work or, failing that, give up and go idle.
+steal :: Sched -> IO ()
+steal q@Sched{ idle, scheds, no=my_no } = do
+  -- printf "cpu %d stealing\n" my_no
+  go scheds
+  where
+    go [] = do m <- newEmptyMVar
+               r <- atomicModifyIORef idle $ \is -> (m:is, is)
+               if length r == numCapabilities - 1
+                  then do
+                     -- printf "cpu %d initiating shutdown\n" my_no
+                     mapM_ (\m -> putMVar m True) r
+                  else do
+                    done <- takeMVar m
+                    if done
+                       then do
+                         -- printf "cpu %d shutting down\n" my_no
+                         return ()
+                       else do
+                         -- printf "cpu %d woken up\n" my_no
+                         go scheds
+    go (x:xs)
+      | no x == my_no = go xs
+      | otherwise     = do
+         r <- atomicModifyIORef (workpool x) $ \ ts ->
+                 case ts of
+                    []     -> ([], Nothing)
+                    (x:xs) -> (xs, Just x)
+         case r of
+           Just t  -> do
+              -- printf "cpu %d got work from cpu %d\n" my_no (no x)
+              sched True q t
+           Nothing -> go xs
+
+-- | If any worker is idle, wake one up and give it work to do.
+pushWork :: Sched -> Trace -> IO ()
+pushWork Sched { workpool, idle } t = do
+  atomicModifyIORef workpool $ \ts -> (t:ts, ())
+  idles <- readIORef idle
+  when (not (null idles)) $ do
+    r <- atomicModifyIORef idle (\is -> case is of
+                                          [] -> ([], return ())
+                                          (i:is) -> (is, putMVar i False))
+    r -- wake one up
